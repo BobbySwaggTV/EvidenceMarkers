fccd6764076b660602ef8e758cc5ff982848c18b
Bobby Swagg <randy.sporleder@outlook.com>
2024-01-28 09:52:30 -0600

    Uploaded Files
    Uploaded Files

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+client_scripts {"client.lua"}
+server_script "server.lua"
+
+data_file 'DLC_ITYP_REQUEST' 'stream/mdxo_evitags.ytyp'
+
+author 'Owen-M (OMSolutions)'
+description 'Crime Scene Evidence Markers'
+version '1.0.0'
